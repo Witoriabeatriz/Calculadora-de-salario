462f8124cf2e79b9ab2b926bec3d6597cf54811f
Witoriabeatriz <witoria.santos@alunoescolassociais.org.br>
2024-03-26 07:22:58 -0300

    css

--- atividade5-php/salario.php
+++ atividade5-php/salario.php
@@ -9,10 +9,15 @@
             align-items: center;
             height: 100vh;
             margin: 0;
+            background-color: #F6C7D8;
         }
         .container {
             width: 50%;
             text-align: center;
+            background-color: white;
+            padding: 20px; 
+            border-radius: 10px; 
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
         }
         table {
             margin: 0 auto; 
